81a23aeff7d9dd3e207128ca21a7d6e68fd5821e
Csar0427 <jeromecesar27@gmail.com>
2025-05-17 13:04:40 +0800

    Added firebase for the login and register

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5C4F0QeOTx88XxBYRKNdqAS3TaUIEJKg",
+  authDomain: "justkick-a1e17.firebaseapp.com",
+  projectId: "justkick-a1e17",
+  storageBucket: "justkick-a1e17.firebasestorage.app",
+  messagingSenderId: "958410816861",
+  appId: "1:958410816861:web:2a9fd16973bd1652383cb4",
+  measurementId: "G-2KRH67SKR9",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+};
