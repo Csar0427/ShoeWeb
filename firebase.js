d6949072f241c5c328cf4322c74c14d585d315b8
Csar0427 <jeromecesar27@gmail.com>
2025-05-17 14:52:20 +0800

    Added Google signin

--- firebase.js
+++ firebase.js
@@ -6,20 +6,34 @@ import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
   signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js"; // Import database functions
 
 const firebaseConfig = {
   apiKey: "AIzaSyC5C4F0QeOTx88XxBYRKNdqAS3TaUIEJKg",
+
   authDomain: "justkick-a1e17.firebaseapp.com",
+
   projectId: "justkick-a1e17",
+
   storageBucket: "justkick-a1e17.firebasestorage.app",
+
   messagingSenderId: "958410816861",
+
   appId: "1:958410816861:web:2a9fd16973bd1652383cb4",
+
   measurementId: "G-2KRH67SKR9",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app); // Initialize the database
 
 export {
   auth,
@@ -27,4 +41,9 @@ export {
   signInWithEmailAndPassword,
   onAuthStateChanged,
   signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  getDatabase, // Export database functions
+  ref,
+  set,
 };
